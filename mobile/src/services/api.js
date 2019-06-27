adcc7eab7d0a7712d93caec6be4365e2923181f9
Cleber Júnior <dwcleb@gmail.com>
2019-06-26 23:00:41 -0300

    remoção da pasta mobile

--- mobile/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: '', // D E V
-  // baseURL: '', //  H O M O L O G A C A O
-  // baseURL: '', // D E P L O Y
-});
-
-export default api;
